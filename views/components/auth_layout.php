478fcead38a8d4c41960ff4ec0c5957d0bd69ebf
aungkyawth3t <aungkyawthethimself@gmail.com>
2026-01-08 18:59:36 +0630

    auth_layout

--- views/components/auth_layout.php
+++ views/components/auth_layout.php
@@ -11,8 +11,7 @@ if (!isset($title)) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= htmlspecialchars($title) ?></title>
   <link rel="icon" type="image/png" href="../../public/assets/extc.png">
-  <link rel="stylesheet" href="../../public/assets/css/input.css?v=<?= time() ?>">
-  <link rel="stylesheet" href="../../public/assets/css/output.css?v=<?= time() ?>">
+  <link rel="stylesheet" href="/src/output.css?v=<?= time() ?>">
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <!-- google fonts -->
--- views/components/copyright.php
+++ views/components/copyright.php
@@ -1,3 +1,3 @@
 <div class="mt-5 text-center text-gray-500 text-xs">
-    &copy; 2025 annchi Production. All right reserved.
+    &copy; 2025 Aung Kyaw Thet. All right reserved.
 </div>
